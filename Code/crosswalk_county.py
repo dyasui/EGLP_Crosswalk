832bd1c3707a33220617154b3b0b8d97997eaada
dyasui <dyasui@uoregon.edu>
2025-03-03 12:40:40 -0800

    working on adding years from 1900 to 1950

--- Code/crosswalk_county.py
+++ Code/crosswalk_county.py
@@ -4,6 +4,7 @@ import os
 from os.path import join,split
 import time
 import numpy as np
+from zipfile import ZipFile
 
 path = split(__file__)[0]
 root = split(path)[0]
@@ -33,13 +34,13 @@ def fix_2010(shp, cw = cw):
     shp['ICPSRST'] = np.nan
     return shp
 
-end_year = '1940'
+end_year = '1950'
 
 ## reading the end year
 ## if you want to use a different end year, change appropriate strings
 ## and variable names
-os.chdir(join(root, "Shapefiles", "nhgis0029_shape"))
-shp_end = gpd.GeoDataFrame.from_file("US_county_" + end_year + '_conflated.shp')
+os.chdir(join(root,"Shapefiles"))
+shp_end = gpd.GeoDataFrame.from_file('US_county_' + end_year + '.shp')
 
 if end_year == '2010':
     shp_end = fix_2010(shp_end)
@@ -57,63 +58,63 @@ for col in cols:
 shp_end.columns = new_cols_end
 
 ## looping through other years
-# other_years = ['1790', '1800', '1810', '1820', '1830', '1840', 
-#              '1850', '1860', '1870', '1880', '1890', '1900', '1910',
-#              '1920', '1930', '1940', '1950', '1960', '1970',
-#              '1980', '1990', '2000', '2010']
-# other_years.remove(end_year)
+other_years = ['1790', '1800', '1810', '1820', '1830', '1840', 
+             '1850', '1860', '1870', '1880', '1890', '1900', '1910',
+             '1920', '1930', '1940', '1950', '1960', '1970',
+             '1980', '1990', '2000', '2010']
+other_years.remove(end_year)
 
-for year in ['1930', '1940']:
-
+for year in other_years:
+    
     start = time.time() ## for testing purposes
-
+    
     ## reading in shapefiles
-    # os.chdir(join(root,"Shapefiles","nhgis0029_shapefile_tl2000_us_county_" + year))
-    shp = gpd.GeoDataFrame.from_file('US_county_' + year + '_conflated.shp')
-
-    # if year == '2010':
-    #     shp = fix_2010(shp)
-
+    os.chdir(join(root,"Shapefiles","nhgis0010_shapefile_tl2000_us_county_" + year))
+    shp = gpd.GeoDataFrame.from_file('US_county_' + year + '.shp')
+    
+    if year == '2010':
+        shp = fix_2010(shp)
+    
     shp['Year'] = year
     shp['area_base'] = shp.area
-
+    
 
     ## intersecting
-    temp = gpd.overlay(shp, shp_end, how = 'intersection', keep_geom_type = False)
-
+    temp = gpd.overlay(shp, shp_end, how = 'intersection')
+    
     ## computing weights 
     temp['area'] = temp.area
     temp['weight'] = temp['area'] / temp['area_base']
-
+    
     ## keeping only relevant variables
     temp = temp[['Year', 'NHGISST', 'NHGISCTY', 'STATENAM', 'NHGISNAM','ICPSRST', 'ICPSRCTY',
                  'area_base', 'NHGISST_' + end_year, 'NHGISCTY_' + end_year, 'STATENAM_' + end_year, 'NHGISNAM_' + end_year, 'ICPSRST_' + end_year, 'ICPSRCTY_' + end_year,
                  'area', 'weight']]
 
     temp = temp[temp['area'] > 10]
-
+    
     ## renormalizing weights
     reweight = temp.groupby(['NHGISCTY', 'NHGISST'])['weight'].sum().reset_index()
     reweight['new_weight'] = reweight['weight']
     reweight = reweight.drop('weight', axis = 1)
-
+    
     temp = temp.merge(reweight, left_on = ['NHGISCTY', 'NHGISST'], right_on = ['NHGISCTY', 'NHGISST'])
     temp['weight'] = temp['weight'] / temp['new_weight']
-
+    
     temp = temp.drop('new_weight', axis =1)
-
+    
     ## making an indicator if the state is in the union
     states_year = states[states[year] ==1]['State']
     temp['US_STATE'] = 0
     temp.loc[temp['STATENAM'].isin(states_year.apply(str.strip)), 'US_STATE'] = 1
 
     ## appending
-    master_df = pd.concat([master_df, temp])
+    master_df = master_df.append(temp)
     print(year, time.time() - start) 
 
 
 ## saving output
-# os.chdir(path)
+os.chdir(path)
 output_filename = 'county_crosswalk_endyr_' + end_year + '.csv'
 master_df.to_csv(output_filename, index= False)
 
